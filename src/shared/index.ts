bf2636e0e0891c547af3feafc55049271bed4daa
Andrii <andriychikulay@gmail.com>
2024-02-01 21:19:39 +0200

    add footer

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,3 +3,4 @@ export { notifyError } from "./notifyError";
 export { setItemToStorage, getItemFromStorage, removeItemFromStorage } from "./utils/localStorage";
 export { Error404 } from "./errors/404";
 export { SearchBtn } from "./SearchBtn";
+export { Footer } from "./Footer";
